491ea243dd1e0dd5ead759b46222742e37923593
Amine Tamar <aminetamar95@gmail.com>
2024-10-14 10:49:56 +0200

    using php variables in html text

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
+<?php 
+$title = "Learn PHP From Scratchhh";
+$welcome ="Welcome To The Course !!!";
 
-<?=
-$msg = "hellasdsao" ;
-echo "<br/>"
 
-
-//this is a single line comment
-
-/*this
-is 
-multiple line
-comment 
-*/
 ?>
-
-<?php
-$msg = "hellasdsao";
-echo $msg;
-?>
-
-
-
 
 
 
@@ -30,18 +14,18 @@ echo $msg;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Learn PHP From Scratch</title>
+    <title> <?= $title ?>  </title>
 </head>
 
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
+            <h1 class="text-3xl font-semibold"><?= $title ?></h1>
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4">Welcome To The Course</h2>
+            <h2 class="text-2xl font-semibold mb-4"><?= $welcome ?></h2>
             <p>In this course, you will learn the fundamentals of the PHP language</p>
         </div>
     </div>
