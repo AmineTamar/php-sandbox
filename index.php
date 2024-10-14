e676f0f21a57c34ba299cce001fa7494f9e5113a
Amine Tamar <aminetamar95@gmail.com>
2024-10-14 11:18:41 +0200

    datatypes

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php 
-$title = "Learn PHP From Scratchhh";
+$title = "Learn PHP From Scratchh";
 $welcome ="Welcome To The Course !!!";
+
+
+// php functions 
+// strings data type
+var_dump($title); // this functions returns variable type and number of characters ex : string(23) "Learn PHP From Scratchh"
+echo "<br/>";
+echo gettype($title);// this function only returns variable type : ex : String
+// integer data type
+echo "<br/>";
+$age = 15;
+var_dump($age); // output = int(15)  returns the  number and type
+echo "<br/>";
+echo gettype($age); // returns type Ex : integer
+
+// php is losly typed , meaning we dont have do sepcify type when creatin a variale , php guess that automaticaly 
+
+$friends = ["asdsa","dsad","dd"];
+echo "<br/>";
+echo $friends; // this does not print the array and throws an error
+echo "<br/>";
+var_dump($friends); // return  of each array element  and array index mumber and value 
+echo "<br/>";
+print_r($friends); //is commonly used to display an array in a human-readable format. It's useful for debugging.
+echo "<br/>";
+echo implode(" -- " , $friends) // returns only the array values , seperated with first argument , in this case " -- "
 
 
 ?>
